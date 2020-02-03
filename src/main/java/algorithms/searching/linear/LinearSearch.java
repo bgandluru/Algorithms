6a4790fab5b73626408a26aa913fffb6613c382b
bgandluru <srisikharam@gmail.com>
2020-02-02 21:54:15 -0800

    linear search

--- src/main/java/algorithms/searching/linear/LinearSearch.java
+++ src/main/java/algorithms/searching/linear/LinearSearch.java
@@ -1,9 +1,8 @@
-package algorithms.searching;
+package algorithms.searching.linear;
 
 public class LinearSearch {
 
-    int linearSearch(int arr[],int element) {
-
+   public int linearSearch(int arr[],int element) {
         for(int i=0;i<arr.length;i++){
             if (arr[i] == element) {
                 return i;
--- src/test/java/algorithms/searching/linear/TestLinearSearch.java
+++ src/test/java/algorithms/searching/linear/TestLinearSearch.java
@@ -1,4 +1,20 @@
 package algorithms.searching.linear;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TestLinearSearch {
+
+    @Test
+    public void testLinearSearch () {
+        int arr[] = { 23,25,26,23,10,3,6,3,41,61,78};
+        LinearSearch ls = new LinearSearch();
+
+        assertTrue(ls.linearSearch(arr,61) != -1);
+        assertTrue(ls.linearSearch(arr,611) == -1);
+
+    }
+
 }
